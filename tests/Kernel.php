2e25e8e9b445f9ebf4bab343f9251e5d4c552aa0
Abdullah Pazarbaşı <mapazarbasi@hotmail.com>
2020-01-26 21:18:40 +0300

    phpunit Kernel

--- tests/Kernel.php
+++ tests/Kernel.php
@@ -2,16 +2,25 @@
 
 namespace SfV\Tests;
 
+use const PHP_VERSION_ID;
+use function dirname;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Bundle\MonologBundle\MonologBundle;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\Routing\RouteCollectionBuilder;
 
 /**
  * Class Kernel
  */
 class Kernel extends BaseKernel
 {
+
+    use MicroKernelTrait;
+
+    private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
 
     /**
      * @inheritDoc
@@ -23,21 +32,46 @@ class Kernel extends BaseKernel
             new MonologBundle(),
         ];
     }
-    
-    /**
-     * @inheritDoc
-     */
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        // TODO: Implement registerContainerConfiguration() method.
-    }
 
     /**
      * @return string
      */
     public function getProjectDir(): string
     {
-        return realpath(__DIR__ . '/../src');
+        return dirname(__DIR__);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function configureRoutes(RouteCollectionBuilder $routes)
+    {
+        $projectDir = $this->getProjectDir();
+        $confDir = $projectDir . '/tests/Resources/config';
+
+        // Route Registrations
+        $routes->import($confDir . '/{routes}/' . $this->environment . '/*' . self::CONFIG_EXTS, '/', 'glob');
+        $routes->import($confDir . '/{routes}/*' . self::CONFIG_EXTS, '/', 'glob');
+        $routes->import($confDir . '/{routes}' . self::CONFIG_EXTS, '/', 'glob');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function configureContainer(ContainerBuilder $cb, LoaderInterface $loader)
+    {
+        $projectDir = $this->getProjectDir();
+        $cb->setParameter('container.dumper.inline_class_loader', PHP_VERSION_ID < 70400 || $this->debug);
+        $cb->setParameter('container.dumper.inline_factories', true);
+        $confDir = $projectDir . '/tests/Resources/config';
+
+        // Package Configurations
+        $loader->load($confDir . '/{packages}/*' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{packages}/' . $this->environment . '/*' . self::CONFIG_EXTS, 'glob');
+
+        // Service Registrations
+        $loader->load($confDir . '/{services}' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{services}_' . $this->environment . self::CONFIG_EXTS, 'glob');
     }
 
 }
